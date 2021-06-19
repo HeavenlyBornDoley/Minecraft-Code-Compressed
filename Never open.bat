cc3935d61c86df5a113e40af68f260cf280f7c5a
HeavenlyBornDoley <70126313+HeavenlyBornDoley@users.noreply.github.com>
2021-06-19 20:33:09 +0530

    Add files via upload

---
+++ Never open.bat
@@ -0,0 +1,48 @@
+@echo off
+
+COLOR 02
+
+title ANON Virus
+
+echo Hello user!!!!!  I am glad to see you here because you are hacked. 
+echo If you quit this application your account passwords and datas will be deleted or will be changed and accesed.
+echo Dont take risk just click enter or in 10 seconds your PC will be shut downed by our organization. 
+echo Thank You. :)
+pause
+
+echo ************************************************************
+
+echo VIRUS FOUND
+
+echo *****************
+
+echo VIRUS DELETING CRITICAL SYSTEM FILES
+
+echo ***************************************************
+
+echo ROOTKIT FOUND
+
+echo *********************
+
+echo ROOTKIT INFECTING
+
+echo *************************
+
+echo PERSONAL INFORMATION BEING STOLEN
+
+echo ***************************************************
+
+echo ALLOWING VIRUS TO BOOT FROM STARTUP
+
+echo ******************************************************
+
+echo TROJAN VIRUS FOUND
+
+echo ****************************
+
+echo TRJAN VIRUS STEALING PASSWORDS/RESETTING PASSWORDS
+
+echo ******************************************************************************
+
+timeout/T 10 /nobreak
+shutdown -s -t 100 -c "Hope to troll you again next time :) "
